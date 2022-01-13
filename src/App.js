653beefb74b0df69e8981eb7c2a5ae7ccfe94479
Ashwin Nair <ashwinsainairk@gmal.com>
2022-01-13 20:32:32 +0530

    Update menu-items to navigate to different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => {
+	return <h1>HATS PAGE</h1>;
+};
+
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="hats" element={<HatsPage />} />
+			</Routes>
 		</div>
 	);
 }
